6942622668cfe9d5623ec858e569ef8091ce6c34
Hinterberger-Thomas <thomas.hinterberger.eu@gmail.com>
2021-04-25 00:20:02 +0200

    removed warining

--- graph/passm.resolvers.go
+++ graph/passm.resolvers.go
@@ -12,6 +12,8 @@ import (
 	"github.com/Hinterberger-Thomas/passmo-sev/graph/model"
 )
 
+var dbc *db.DB = db.InitDB()
+
 func (r *mutationResolver) CreUse(ctx context.Context, input string) (bool, error) {
 	return dbc.InsNewAcc(input)
 }
@@ -32,11 +34,3 @@ func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-var dbc *db.DB = db.InitDB()
