25edaae573b16b5a62040dee16cc43bd0e54da44
Hinterberger-Thomas <thomas.hinterberger.eu@gmail.com>
2021-05-03 14:34:08 +0100

    new enter

--- graph/passm.resolvers.go
+++ graph/passm.resolvers.go
@@ -30,10 +30,16 @@ func (r *queryResolver) Passwords(ctx context.Context) ([]*model.Account, error)
 }
 
 // Mutation returns generated.MutationResolver implementation.
-func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+func (r *Resolver) Mutation() generated.MutationResolver
+{
+	return &mutationResolver{r}
+}
 
 // Query returns generated.QueryResolver implementation.
-func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+func (r *Resolver) Query() generated.QueryResolver
+{
+	return &queryResolver{r}
+}
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
